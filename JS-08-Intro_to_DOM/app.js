c992170d4edc82ca9c3e993afc87c1dd2aa7155d
Shiv Singh <shivsinghcse@gmail.com>
2024-12-05 20:09:41 +0530

    task-01 Append hrllo in body  completed

--- JS-08-Intro_to_DOM/app.js
+++ JS-08-Intro_to_DOM/app.js
@@ -1,1 +1,26 @@
-alert()
+//------ using onclick
+
+// function addText() {
+//     // let body = document.body;
+//     let body = document.querySelector('body');
+//     body.innerHTML = '<h1>Hello</h1>';
+//     // body.innerText = '<h1>Hello</h1>';
+// }
+
+// using addEventListener
+
+const btnEl = document.querySelector('button');
+// alert(btn)
+console.log(btnEl);
+btnEl.addEventListener('click', function () {
+    const bodyEl = document.querySelector('body');
+    const heading = document.createElement('h1');
+    // heading.innerText = 'Hello from addEventListener()';
+    const headingText = document.createTextNode(
+        'Hello from addEventListener()'
+    );
+    heading.appendChild(headingText);
+    bodyEl.appendChild(heading);
+});
+
+// removeEventListener - to remove the attached event
